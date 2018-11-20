6274e7c05baebcecf55cfe85eb58d83c0c47af23
svyshpolskyi <serhii.vyshpolskyi@bazaarvoice.com>
2018-11-20 11:30:43 +0200

    added setinterval to server.js to to keep app always alive

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/betapp'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/betapp/index.html'));
-});
+setInterval(function() {
+    app.get('/*', function(req,res) {
+        res.sendFile(path.join(__dirname+'/dist/betapp/index.html'));
+    });
+}, 300000);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
