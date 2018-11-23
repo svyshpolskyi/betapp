908295b36286c669828ee847f1fe1352f4dba204
svyshpolskyi <serhii.vyshpolskyi@bazaarvoice.com>
2018-11-23 10:03:06 +0200

    always online fix for server.js, added new header component, added bootstrap and jquery

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require("http");
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/betapp'));
 
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/betapp/index.html'));
+});
+
 setInterval(function() {
-    app.get('/*', function(req,res) {
-        res.sendFile(path.join(__dirname+'/dist/betapp/index.html'));
-    });
-}, 300000);
+    http.get("https://bettingapp.herokuapp.com");
+   console.log("--ping--");
+}, 300000); // every 5 minutes (300000)
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
