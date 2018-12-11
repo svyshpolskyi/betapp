2a5f6a17aae937523045182ff0cf3576d4f55b6a
svyshpolskyi <serhii.vyshpolskyi@bazaarvoice.com>
2018-12-11 12:04:37 +0200

    server.js modifications

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import ping from "./ping"
+const ping = require("./ping.js");
 //Install express server
 const express = require('express');
 const path = require('path');
-const http = require("http");
 
 const app = express();
 
@@ -14,7 +13,7 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/betapp/index.html'));
 });
 
-ping();
+ping.ping();
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
